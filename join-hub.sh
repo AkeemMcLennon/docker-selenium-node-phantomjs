84bea2486bf9af1867221086c1a1c62bdf7f2048
Nick Ilkevich <vbox.nick@gmail.com>
2016-07-30 11:29:37 +0200

    retry connecting to hub in case it hasn't started yet

--- join-hub.sh
+++ join-hub.sh
@@ -1,1 +1,7 @@
-/usr/bin/phantomjs --webdriver `hostname -I | awk '{print $1}'`:8080 ${PHANTOMJS_OPTS} --webdriver-selenium-grid-hub http://hub:4444
+#!/bin/bash
+HUB=${SELENIUM_HUB_ADDRESS:-hub:4444}
+until /usr/bin/phantomjs --webdriver `hostname -I | awk '{print $1}'`:8080 ${PHANTOMJS_OPTS} --webdriver-selenium-grid-hub http://${HUB}
+do
+  echo "Trying to connect to ${HUB}"
+  sleep 3
+done
